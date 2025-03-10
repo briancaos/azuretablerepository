b5ec69340a1746eb3e00fa0e58e4b273d5f00d99
Brian Pedersen <bp@pentia.dk>
2025-03-10 11:10:00 +0100

    Initial checkin

---
+++ AzureTableBaseRepository.cs
@@ -0,0 +1,177 @@
+﻿using Azure;
+using Azure.Data.Tables;
+
+namespace BrianCaos.Foundation
+{
+  public abstract class AzureTableBaseRepository<T> where T : class, ITableEntity
+  {
+    private readonly TableServiceClient _tableServiceClient;
+    private readonly TableClient _tableClient;
+
+    public AzureTableBaseRepository(string tableName, TableServiceClient tableServiceClient)
+    {
+      _tableServiceClient = tableServiceClient;
+      _tableClient = _tableServiceClient.GetTableClient(tableName);
+      _tableClient.CreateIfNotExists();
+    } 
+
+    /// <summary>
+    /// Adds a Table Entity of type <see cref="T"/> into the Table.
+    /// </summary>
+    /// <param name="entity">A custom model type that implements ITableEntity or an instance of TableEntity</param>
+    /// <returns></returns>
+    public async Task AddAsync(T entity) 
+    {
+      await _tableClient.AddEntityAsync(entity);
+    }
+
+    /// <summary>
+    /// Adds a list of Table Entity of type <see cref="T"/> into the Table.
+    /// </summary>
+    /// <param name="entities">A list of custom model types that implements ITableEntity or an instance of TableEntity</param>
+    /// <returns></returns>
+    public async Task AddAsync(IEnumerable<T> entities) 
+    {
+      await SubmitTransactionAsync(entities, TableTransactionActionType.Add);
+    }
+
+    /// <summary>
+    /// Unconditionally replaces the specified table entity of type <see cref="T"/>, if it exists. 
+    /// </summary>
+    /// <param name="entity">A custom model type that implements ITableEntity or an instance of TableEntity</param>
+    /// <returns></returns>
+    public async Task UpdateAsync(T entity) 
+    {
+      await _tableClient.UpdateEntityAsync(entity, Azure.ETag.All, TableUpdateMode.Replace);
+    }
+
+    /// <summary>
+    /// Updates the specified table entity of type <see cref="T"/>, if it exists. 
+    /// If the mode is Replace, the entity will be replaced. 
+    /// If the mode is Merge, the property values present in the entity 
+    /// will be merged with the existing entity.
+    /// </summary>
+    /// <param name="entity">A custom model type that implements ITableEntity or an instance of TableEntity</param>
+    /// <param name="ifMatch">
+    /// The If-Match value to be used for optimistic concurrency. 
+    /// If All is specified, the operation will be executed unconditionally. 
+    /// If the ETag value is specified, the operation will fail with a status 
+    /// of 412 (Precondition Failed) if the ETag value of the entity in the 
+    /// table does not match.
+    /// </param>
+    /// <param name="tableUpdateMode">Determines the behavior of the Update operation.</param>
+    /// <returns></returns>
+    public async Task UpdateAsync(T entity, ETag ifMatch, TableUpdateMode tableUpdateMode) 
+    {
+      await _tableClient.UpdateEntityAsync(entity, ifMatch, tableUpdateMode);
+    }
+
+    /// <summary>
+    /// Unconditionally replaces the specified list table entities of type <see cref="T"/>, if they exists. 
+    /// </summary>
+    /// <param name="entities">A list of custom model types that implements ITableEntity or an instance of TableEntity</param>
+    /// <returns></returns>
+    public async Task UpdateAsync(IEnumerable<T> entities) 
+    {
+      await SubmitTransactionAsync(entities, TableTransactionActionType.UpdateReplace);
+    }
+
+    /// <summary>
+    /// Replaces the specified table entity of type <see cref="T"/>, if it exists. 
+    /// Creates the entity if it does not exist.
+    /// </summary>
+    /// <param name="entity">A custom model type that implements ITableEntity or an instance of TableEntity</param>
+    /// <returns></returns>
+    public async Task UpsertAsync(T entity) 
+    {
+      await _tableClient.UpsertEntityAsync(entity);
+    }
+
+    /// <summary>
+    /// Replaces the specified list of table entity of type <see cref="T"/>, if it exists. 
+    /// Creates the entities if they does not exist.
+    /// </summary>
+    /// <param name="entities">A list of custom model types that implements ITableEntity or an instance of TableEntity</param>
+    /// <returns></returns>
+    public async Task UpsertAsync(IEnumerable<T> entities) 
+    {
+      await SubmitTransactionAsync(entities, TableTransactionActionType.UpsertReplace);
+    }
+
+    /// <summary>
+    /// Deletes the specified table entity.
+    /// </summary>
+    /// <param name="partitionKey">The partitionKey that identifies the table entity/param>
+    /// <param name="rowKey">The rowKey that identifies the table entity</param>
+    /// <returns></returns>
+    public async Task DeleteAsync(string partitionKey, string rowKey)
+    {
+      await _tableClient.DeleteEntityAsync(partitionKey, rowKey);
+    }
+
+    /// <summary>
+    /// Deletes the list of specified table entities
+    /// </summary>
+    /// <param name="entities">A list of custom model types that implements ITableEntity or an instance of TableEntity</param>
+    /// <returns></returns>
+    public async Task DeleteAsync(IEnumerable<T> entities) 
+    {
+      await SubmitTransactionAsync(entities, TableTransactionActionType.Delete);
+    }
+
+    /// <summary>
+    /// Gets the specified table entity of type <see cref="T"/>
+    /// </summary>
+    /// <param name="partitionKey">The partitionKey that identifies the table entity/param>
+    /// <param name="rowKey">The rowKey that identifies the table entity</param>
+    /// <returns></returns>
+    public async Task<T> GetAsync(string partitionKey, string rowKey) 
+    {
+      return await _tableClient.GetEntityAsync<T>(partitionKey, rowKey);
+    }
+
+    /// <summary>
+    /// Get a list of table entities of type <see cref="T"/>
+    /// </summary>
+    /// <param name="filter">Query that specifies which entities to return</param>
+    /// <returns>A <see cref="IEnumerable"/> list of entities matcing the query specified</returns>
+    public async Task<IEnumerable<T>> GetAsync(System.Linq.Expressions.Expression<Func<T, bool>> filter) 
+    {
+      string? continuationToken = null;
+      List<T> allMessages = new List<T>();
+      do
+      {
+        var page = await GetAsync(filter, continuationToken);
+        continuationToken = page.Item1;
+        allMessages.AddRange(page.Item2);
+      } while (continuationToken != null);
+
+      return allMessages;
+    }
+
+    private async Task<Tuple<string, IEnumerable<T>>?> GetAsync(System.Linq.Expressions.Expression<Func<T, bool>> filter, string? continuationToken)
+    {
+      var messages = _tableClient.QueryAsync(filter, maxPerPage: 1000);
+
+      await foreach (var page in messages.AsPages(continuationToken))
+      {
+        return Tuple.Create<string, IEnumerable<T>>(page.ContinuationToken, page.Values);
+      }
+      return null;
+    }
+
+    private async Task SubmitTransactionAsync(IEnumerable<T> entities, TableTransactionActionType tableTransactionActionType)
+    {
+      foreach (var batch in entities.Chunk(100))
+      {
+        List<TableTransactionAction> action = new List<TableTransactionAction>();
+        foreach (var entity in batch)
+        {
+          action.Add(new TableTransactionAction(tableTransactionActionType, entity));
+        }
+        await _tableClient.SubmitTransactionAsync(action);
+      }
+    }
+
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,182 @@
-# azuretablerepository
-A simple Azure Table base class implementation, simplifying CRUD operations on Azure Table
+# AzureTableRepository
+
+A simple Azure Table base class implementation, simplifying CRUD operations on Azure Table.
+
+- Use strong types.
+- Read an entire table without paging.
+- No size limit when bulk insert, update, delete. 
+
+# How to Setup the base class
+
+## STEP 1: Implement a ITableEntity class
+
+This class is the contents of your table. This is an example class:
+
+```csharp
+  public class MyEntity : ITableEntity
+  {
+    /// <summary>
+    /// Mandatory field
+    /// </summary>
+    public string? PartitionKey { get; set; }
+    /// <summary>
+    /// Mandatory field
+    /// </summary>
+    public string? RowKey { get; set; }
+    /// <summary>
+    /// Mandatory field
+    /// </summary>
+    public DateTimeOffset? Timestamp { get; set; } = default!;
+    /// <summary>
+    /// Mandatory field
+    /// </summary>
+    public ETag ETag { get; set; } = default!;
+
+    /// <summary>
+    /// Custom table field
+    /// </summary>
+    public string? Value1 { get; set; } 
+  }
+```
+
+## STEP 2: Create a concrete implementation of the class
+
+In this class you specify the name of the table to store your "MyEntity". And you give base class the name of the ITableEntity to store in that table.
+
+```csharp
+using Azure.Data.Tables;
+using WasteNotifications.Foundation;
+
+namespace MyCode
+{
+    public class MyRepository : AzureTableBaseRepository<MyEntity>
+    {
+        public SmsBatchesRepository(TableServiceClient tableServiceClient) : base("MyTable", tableServiceClient)
+        {
+        }
+    }
+}
+```
+
+## STEP 3: Setup dependency injection in Program.cs
+
+```csharp
+// Creating a connection to a storage account containing the 
+// Azure Table
+builder.Services.AddAzureClients(context => context.AddTableServiceClient(builder.Configuration.GetConnectionString("StorageAccount")));
+// Allowing for depencency injection
+builder.Services.AddTransient<MyRepository>();
+```
+
+# HOW TO USE THE REPOSITORY
+
+## Injecting class into other class
+
+```csharp
+public class MyClass 
+{
+    private MyRepository _myRepository;
+
+    public MyClass(MyRepository myRepository)
+    {
+        _myRepository = myRepository;
+    }
+
+    ...
+    ...
+}
+```
+
+## Create, Update, or Upsert 1 entity
+
+```csharp
+var entity = new MyEntity() 
+{ 
+    PartitionKey = "partition",
+    RowKey = "row001",
+    Value1 = "myvalue"
+};
+
+// Adds a new row. Will fail if the partitionkey/rowkey already exists
+await _myRepository.AddAsync(entity);
+
+// Updates a row. Will fail if the partitionkey/rowkey does not exist
+await _myRepository.UpdateAsync(entity);
+
+// If entity exists, updates the entity. If not, a new is created
+await _myRepository.UpsertAsync(entity);
+```
+
+## Handle concurrency issues on update
+
+You can use the UpdateAsync(T entity, ETag ifMatch, TableUpdateMode tableUpdateMode) method to handle concurrency issues:
+
+```csharp
+int maxRetries = 10;
+int retryCount = 0;
+while (retryCount < maxRetries)
+{
+    try
+    {
+        var entity = _myRepository.GetAsync("partition", "row001");
+        entity.Value1 = entity.Value1 + "1";
+        await _myRepository.UpdateAsync(entity, entity.ETag, TableUpdateMode.Replace);
+        break;
+    }
+    // 412 = Precondition Failed (ETag mismatch)
+    catch (RequestFailedException ex) when (ex.Status == 412) 
+    {
+        retryCount++;
+        if (retryCount == maxRetries)
+        {
+            throw new Exception("Cannot update entity");
+        }
+        else
+        {
+            // Exponential backoff (0.5s, 1s, 1.5s, ...)
+            await Task.Delay(500 * retryCount);
+        }
+    }
+}
+```
+
+## Create, Update, or Upsert many entities
+
+In these methods, you don't need to worry about the insert limits of Azure Tables, the class will chunk your array for you
+
+```csharp
+List<MyEntity> entityList = new List<MyEntity>();
+
+for (int i=0;i<100;i++)
+{
+    var entity = new MyEntity() 
+    { 
+        PartitionKey = "partition",
+        RowKey = "row" + i,
+        Value1 = "myvalue"
+    };
+}
+
+// Adds new rows. Will fail if the partitionkeys/rowkeys already exists
+await _myRepository.AddAsync(entityList);
+
+// Updates rows. Will fail if the partitionkeys/rowkeys does not exist
+await _myRepository.UpdateAsync(entityList);
+
+// If entities exists, updates the entities. If not, new entities are created
+await _myRepository.UpsertAsync(entityList);
+```
+
+## Read entities
+
+```csharp
+// One specific entity
+var entity = await _myRepository.GetAsync("partition", "row001");
+
+// Using LINQ to query table. Returns all entities matching the 
+// query.
+var entities = await _myRepository.GetAsync(e => e.PartitionKey == "partition");
+```
+
+
+
